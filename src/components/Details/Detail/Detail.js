8869421f857dfd67bfb1d3ea3111d4a0dc04ff47
OsmanRana <osmancmc@yahoo.com>
2021-10-01 11:11:23 +0600

    back button added

--- src/components/Details/Detail/Detail.js
+++ src/components/Details/Detail/Detail.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
+import { useHistory } from 'react-router';
 
 const Detail = (props) => {
-    const {strMeal, strInstructions} = props.element;
+    const {strMeal, strInstructions, strMealThumb} = props.element;
+    const history = useHistory ()
+
+    const handleClick = () => {
+        history.push("/restaurent")
+    }
     return (
         <div className="container">
             <h2>{strMeal}</h2>
+            <img src = {strMealThumb} alt =""/>
             <h4>Instruction</h4>
             <p>{strInstructions}</p>
+            <Button onClick = {handleClick}>Back</Button>
         </div>
     );
 };
